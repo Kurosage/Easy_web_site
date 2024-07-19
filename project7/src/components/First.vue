2b6da134b5b23fcfb023d2db27f1546e20e74f84
Kamila Gilfanova <89061273+Kurosage@users.noreply.github.com>
2024-07-19 11:42:14 +0000

    p71

--- project7/src/components/First.vue
+++ project7/src/components/First.vue
@@ -1,37 +1,62 @@
 <template>
   <div class="container">
-    <div class="search-panel">
-      <div class="icon" @click="openSearchPanel()" :class="{isActivated:active}"> 
+    <div class="search-panel" :class="{isActivated:active}">
+      <input class="inputText" placeholder="Search..." type="text" >
+      <div class="icon" @click="openSearchPanel()">
         <i class="fa fa-search fa-3x" style="color: black;" aria-hidden="true"></i>
       </div>
-      
     </div>
   </div>
 </template>
+
 <script setup>
 import {ref} from "vue"
 const active = ref(false)
+
 function openSearchPanel(){
   active.value = !active.value
+  const input = document.querySelector('.inputText');
+  if (active.value == true){
+    input.style.zIndex = 1;
+    input.style.width = '220px'
+  }else{
+    input.style.zIndex = -1;
+    input.style.width = '0px'
+  }
 }
 </script>
+
 <style scoped>
 .search-panel{
-  width:70px;
-  height: 70px;
+  width:80px;
+  height: 90px;
   background-color: #fff;
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: end;
   cursor:pointer;
-  transition:linear 1s;
+  transition: width 0.1s linear ;
 }
 .icon{
-  padding:10px;
+  padding:10px 15px 10px 10px;
 }
-.isActiveated{
+.isActivated{
   width:300px;
-  background-color: #f234;
-  /* transition:linear 1s; */
+  transition: width 0.1s linear;
+}
+.inputText{
+  height:50px;
+  width:0px;
+  border:none;
+  padding: 5px;
+  font-size: 25px;
+  transition: width 0.1s linear ;
+  overflow: hidden;
+  z-index: -1;
+}
+.inputText:focus{
+  border: none;
+  outline: none;
+
 }
 </style>
