ca6aa5980c58d353bac3405350e9b4f29e303d2b
Kamila Gilfanova <89061273+Kurosage@users.noreply.github.com>
2024-07-19 13:43:33 +0000

    p72

--- project7/src/components/Second.vue
+++ project7/src/components/Second.vue
@@ -1,8 +1,43 @@
 <template>
-  <p>hii2ii</p>
+  <div class="container">
+    <img class="picture" ref="picture" src="../assets/picture.jpg" @load="startLoading" alt="nature">
+    <div class="loader" ref="loader"><p>{{progress}}%</p></div>
+  </div>
+  
 </template>
 <script setup>
+import { onMounted,ref } from 'vue';
+const loading = ref(true)
+const progress= ref(0)
+const loader = ref(null)
+const picture = ref(null)
+function startLoading() {
+  const timer = setInterval(() => {
+    
+    loader.value.style.opacity = 1 - progress.value / 100
+    picture.value.style.filter = `blur(${10 - progress.value / 10}px)`
+    progress.value += 1
+    
+    if (progress.value >= 100) {
+      clearInterval(timer)
+      loading.value = false
+    }
+  }, 50)
+}
 </script>
 <style scoped>
-  
+  .picture{
+    width:100%;
+    height:80%;
+    object-fit: cover;
+    filter: blur(10px);
+  }
+  .loader{
+    position:absolute;
+    
+    font-size:80px;
+    color: #fff;
+    opacity:1;
+
+  }
 </style>
