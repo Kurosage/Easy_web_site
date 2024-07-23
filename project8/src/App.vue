d0d06fb7338f2b9c9bd31ec8348297f43de6f9e2
Kamila Gilfanova <89061273+Kurosage@users.noreply.github.com>
2024-07-23 13:34:03 +0000

    p81

--- project8/src/App.vue
+++ project8/src/App.vue
@@ -2,12 +2,31 @@
 
 <template>
   <div class="container">
-    
+    <div class="left-container">eee</div>
+    <div class="right-container">ddd</div>
   </div>
 </template>
 <script setup>
 
 </script>
 <style scoped>
-
+.container{
+    width: 100%;
+    height: 100vh;
+    
+}
+.left-container{
+   float:left; 
+   height: 100vh;
+   width:50%;
+   background-image: url("./assets/1p.jpg");
+   background-color: #66439e;
+}
+.right-container{
+   float:left; 
+   height: 100vh;
+   width:50%;
+   background-image: url("./assets/2p.jpg");
+   background-color: #313131;
+}
 </style>
